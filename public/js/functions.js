f141952b51aada46efd3110f0a45327a1be5c65d
ilyes-guy <ilyes.guy@gmail.com>
2022-10-29 21:30:16 +0100

    afezf

--- public/js/functions.js
+++ public/js/functions.js
@@ -47,3 +47,97 @@ const define_hovered_row = (target) => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//declare html comps
+const table_element = '<div id="remove_on_error" class="border-y border-primary w-full flex flex-row bg-gray-800 hover:bg-gray-600 h-10 items-center px-2 cursor-pointer table_row"> \
+                                <div class="flex flex-row space-x-2 w-2/3"> \
+                                    <div class="border hover:border-white rounded-sm border-transparent p-0.5 self-center">     \
+                                        <div class="h-3 w-3 bg-white rounded-sm self-center "></div> \
+                                    </div> \
+                                    <div><input id="new_task_name" type="text" class=" bg-transparent"></div> \
+                                    <div class="toggle_sub_tasks this_hidden_icons rounded-md border border-gray-800 w-5 h-5 hidden"><%= workspaces[i].lists[j].tasks[k].sub_tasks.length %></div> \
+                                    <div class="toggle_tags_modal this_hidden_icons rounded-md border border-gray-800 w-5 h-5 hidden">i</div> \
+                                    <div class="this_hidden_icons rounded-md border border-gray-800 w-5 h-5 hidden">i</div> \
+                                </div> \
+                                <div class="flex flex-row w-1/3"> \
+                                    <div class="w-1/4 text-center">temp</div> \
+                                    <div class="w-1/4 text-center">temp</div> \
+                                    <div class="w-1/4 text-center">temp</div> \
+                                    <div class="w-1/4 text-center">temp</div> \
+                                </div> \
+                            </div> '
+
+
+
+const sub_table_element = '<div id="remove_on_error" class="border-y border-primary w-full flex flex-row bg-gray-800 hover:bg-gray-600 h-10 items-center px-2 cursor-pointer table_row"> \
+<div class="flex flex-row space-x-2 w-2/3 h-full items-center">  \
+        <div class="w-10 h-full flex flex-row">  \
+            <div class="w-1/4 h-full "></div>  \
+            <div class="w-3/4 h-full ">  \
+                <div class="w-full h-1/2  border-b border-l rounded-bl-lg"></div>  \
+            </div>  \
+        </div>  \
+    <div class="border hover:border-white rounded-sm border-transparent p-0.5 self-center">    \
+        <div class="h-3 w-3 bg-white rounded-sm self-center "></div>  \
+    </div>  \
+    <div><input id="new_task_name" type="text" class=" bg-transparent"></div>\
+    <div class="this_hidden_icons rounded-md border border-gray-800 w-5 h-5 hidden">i</div>\
+    <div class="this_hidden_icons rounded-md border border-gray-800 w-5 h-5 hidden">i</div>\
+    <div class="this_hidden_icons rounded-md border border-gray-800 w-5 h-5 hidden">i</div>\
+</div>\
+<div class="flex flex-row w-1/3">\
+    <div class="w-1/4 text-center">ico</div>\
+    <div class="w-1/4 text-center">ico</div>\
+    <div class="w-1/4 text-center">icp</div>\
+    <div class="w-1/4 text-center">ico</div>\
+</div>\
+</div> '
+
+
+const middle_sub_task = ' <div class="w-10 h-full flex flex-row divide-x"> \
+    <div class="w-1/4 h-full "></div> \
+    <div class="w-3/4 h-full "> \
+    <div class="w-full h-1/2  border-b rounded-bl-lg"></div> \
+    </div> \
+</div>'
+
+
+const final_sub_task = '<div class="w-10 h-full flex flex-row "> \
+    <div class="w-1/4 h-full "></div> \
+    <div class="w-3/4 h-full "> \
+        <div class="w-full h-1/2  border-b border-l rounded-bl-lg"></div> \
+    </div> \
+</div>'
